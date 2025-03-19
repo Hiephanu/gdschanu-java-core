6b42a0ddbfd49969532f5672122e7d7032a81deb
Hiephanu <doanvanhiep230903@gmail.com>
2025-03-19 20:27:17 +0700

    [core] print test on terminal

--- oop/Main.java
+++ oop/Main.java
@@ -3,6 +3,7 @@ package oop;
 public class Main {
     public static void main(String[] args) {
         Encap encap = new Encap();
+        System.out.println("Test");
 //        encap.b;
 //        encap.a;
     }
