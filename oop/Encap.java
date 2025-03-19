9074efab3132afdc77884b9c38249986c02a193e
Hiephanu <doanvanhiep230903@gmail.com>
2025-03-19 20:16:14 +0700

    [core] init code

---
+++ oop/Encap.java
@@ -0,0 +1,7 @@
+package oop;
+
+//private không truy cập được t bên ngoài
+public class Encap {
+    private int a;
+    public int b;
+}
---
+++ oop/Main.java
@@ -0,0 +1,9 @@
+package oop;
+
+public class Main {
+    public static void main(String[] args) {
+        Encap encap = new Encap();
+//        encap.b;
+//        encap.a;
+    }
+}
